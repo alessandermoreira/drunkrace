136681a71bc460000d8d8382b7397715f92744f5
Guilherme Thales <guilhermevtog@hotmail.com>
2017-05-17 14:19:07 -0300

    arquivos jogo drunkrace

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    public Animator Anim;
+    public Rigidbody2D PlayerRigidbody;
+    public int forcejump;
+
+    public bool jump;
+    public bool bebado;
+
+    //VERIFICA O CHAO
+    public Transform GroundCheck;
+    public bool grounded;
+    public LayerMask WhatIsGround;
+
+    //colisor
+    public Transform colisor;
+
+    
+
+
+    void Start () {
+        
+	}
+	
+	
+	void Update () {
+        //Click botão de pular
+        if (Input.GetButtonDown("Jump") && grounded == true)
+        {
+            PlayerRigidbody.AddForce(new Vector2(0, forcejump));
+            
+        }
+
+        if (Input.GetButtonDown("Bebado"))
+        {
+
+            bebado = true;
+        }
+
+        grounded = Physics2D.OverlapCircle(GroundCheck.position, 0.2f, WhatIsGround);
+
+        Anim.SetBool("jump", !grounded);
+
+    }
+
+    private void OnTriggerEnter2D()
+    {
+        
+    }
+
+}
---
+++ Assets/Scripts/spawnController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnController : MonoBehaviour {
+
+    public GameObject barreiraPrefab; //Objeto a ser spawnado
+    public float rateSpawn;           //Intervalo de spawn
+    public float currentTime;         //Armazena o tempo entre um spawn e outro
+    private int posicao;
+    private float y;
+
+	// Use this for initialization
+	void Start () {
+        currentTime = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentTime += Time.deltaTime;
+        if(currentTime >= rateSpawn)
+        {
+            currentTime = 0;
+            posicao = Random.Range(1, 100);
+            
+            GameObject tempPrefab = Instantiate(barreiraPrefab) as GameObject;
+            tempPrefab.transform.position = new Vector3(transform.position.x, tempPrefab.transform.position.y, tempPrefab.transform.position.z);
+        }
+	}
+}
